5a1c6f9191c506ca6c3d49f548d582dff8a6c6d3
Avi1702 <avinashroy1702@gmail.com>
2023-02-07 12:43:39 +0530

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+// import logo from './logo.svg';
+import './App.css';
+import Navbar from './components/Navbar';
+import Filter from './pages/Filter';
+
+function App() {
+  return (
+    <div className="App">
+        <Navbar/>
+        <Filter/>
+    </div>
+  );
+}
+
+export default App;
